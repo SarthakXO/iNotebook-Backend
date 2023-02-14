d4f42a0ad618b96197d448f222c8cb6eb2a23e7f
Sarthak <sarthakbhatt@live.com>
2023-02-14 07:02:37 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4";
+
+const connectToMongo = async () => {
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to Mongo Successfully")
+    })
+};
+
+
+module.exports=connectToMongo
